9c095306f88f6b66a6c6a8c5cef6dff223a24bf9
Stéphane Klein <contact@stephane-klein.info>
2017-04-19 12:53:16 +0200

    Remove useless cleanup script

--- scripts/minimize.sh
+++ scripts/minimize.sh
@@ -26,4 +26,9 @@ dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
+
+# Clean up
+apt-get --yes autoremove
+apt-get --yes clean
+
 sync
